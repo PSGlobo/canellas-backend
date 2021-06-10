3a56174b366c712b2623801a05aa2a571f07764b
Eduardo Canellas <eduardocanellas98@gmail.com>
2021-06-10 12:42:54 -0300

    refactor: move `health_check` test to a separate module

---
+++ tests/api/health_check.rs
@@ -0,0 +1,17 @@
+use reqwest::Method;
+use serde_json::Value;
+
+use crate::helpers;
+
+#[actix_rt::test]
+async fn health_check_works() {
+    let response = helpers::test_client()
+        .await
+        .request(Method::GET, "health_check", &Value::Null)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
--- tests/api/main.rs
+++ tests/api/main.rs
@@ -1,17 +1,2 @@
-use reqwest::Method;
-use serde_json::Value;
-
+mod health_check;
 pub mod helpers;
-
-#[actix_rt::test]
-async fn health_check_works() {
-    let response = helpers::test_client()
-        .await
-        .request(Method::GET, "health_check", &Value::Null)
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
-    assert!(response.status().is_success());
-    assert_eq!(Some(0), response.content_length());
-}
